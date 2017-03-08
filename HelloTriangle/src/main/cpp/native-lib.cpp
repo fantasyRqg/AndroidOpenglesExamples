2182129f91a2aeac8a583a28db85b5e054fb22be
rqg <ranqingguo90@qq.com>
2017-03-08 13:18:35 +0800

    test glm

--- HelloTriangle/src/main/cpp/native-lib.cpp
+++ HelloTriangle/src/main/cpp/native-lib.cpp
@@ -1,11 +1,22 @@
 #include <jni.h>
 #include <string>
+#include <glm/glm.hpp>
+
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/gtx/string_cast.hpp>
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_rqg_fantasy_androidopenglexamples_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+
+    glm::mat4 I(1.0f);
+
+    std::string hello = "Hello from C++" + glm::to_string(I);
     return env->NewStringUTF(hello.c_str());
 }
+
+
+#undef GLM_ENABLE_EXPERIMENTAL
